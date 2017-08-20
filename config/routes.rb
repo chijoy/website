78a7249cac59d5ae322e3305ace1278829a51d06
Joy <joymccaffreychicago@gmail.com>
2017-08-20 17:43:46 -0500

    Add onclick function to javascript page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   get '/pages/:id/edit' => 'pages#edit'
   patch '/pages/:id' => 'pages#update'
   delete '/pages/:id' => 'pages#destroy'
+
+  get '/javascripts' => 'javascripts#index'
+  get '/javascripts/new' => 'javascripts#new'
+  post '/javascripts' => 'javascripts#create'
+  get '/javascripts/:id' => 'javascripts#show'
+  get '/javascripts/:id/edit' => 'javascripts#edit'
+  patch '/javascripts/:id' => 'javascripts#update'
+  delete '/javascripts/:id' => 'javascripts#destroy'
 end
